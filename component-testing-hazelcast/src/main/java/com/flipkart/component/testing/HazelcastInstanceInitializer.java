ce3ab6a821841350b26f739d742f357693903772
dipika.g <dipika.g@flipkart.com>
2020-04-30 14:18:03 +0530

    HazelCast IP Fix (#117)
    * added localhost as host ip in config

--- component-testing-hazelcast/src/main/java/com/flipkart/component/testing/HazelcastInstanceInitializer.java
+++ component-testing-hazelcast/src/main/java/com/flipkart/component/testing/HazelcastInstanceInitializer.java
@@ -7,7 +7,6 @@ import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.nio.serialization.Serializer;
-import com.hazelcast.spi.SimpleMemberAddressProvider;
 import com.hazelcast.spi.properties.GroupProperty;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryProperties;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryStrategyFactory;
@@ -48,9 +47,8 @@ public class HazelcastInstanceInitializer {
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
         config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
         config.getNetworkConfig().getJoin().getAwsConfig().setEnabled(false);
-        config.getNetworkConfig().getMemberAddressProviderConfig().setEnabled(true);
-        config.getNetworkConfig().getMemberAddressProviderConfig().setImplementation(new SimpleMemberAddressProvider());
 
+        config.setProperty("hazelcast.local.localAddress","127.0.0.1");
         config.setProperty(GroupProperty.DISCOVERY_SPI_ENABLED.getName(), "true");
         DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(new ZookeeperDiscoveryStrategyFactory());
         discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_URL.key(), String.format("localhost:%d", 2181));
